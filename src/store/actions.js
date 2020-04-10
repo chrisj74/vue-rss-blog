b58c061c8cc69e0a3ed13b507247faabbf6eca02
Emanuele <ema987@users.noreply.github.com>
2020-04-10 11:23:29 +0200

    Add posts limit parameter for each endpoint (#5)
    * Add posts limit parameter for each endpoint
    
    Using this parameter it will be possible to limit the number of posts fetched for each endpoint.
    To keep backward compatibility, not using any limit will cause all the posts to be fetched
    
    * Use pipe as feed url and items count separator
    
    * Check rssItemsCount param is a valid number

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,17 +3,40 @@ export function someAction (context) {
 }
 */
 import axios from "axios";
-export function getPosts({ commit }, rssUrl) {
-  const rssArray = rssUrl.replace(/ /g, "").split(",");
-  rssArray.forEach(rss => {
+export function getPosts({ commit }, rss) {
+
+  const rssArray = rss.split(",");
+  const rssUrlsWithOptionsArray = rssArray.map(rss => {
+    const rssUrlWithOptions = rss.split("|");
+    const rssItemsCountParam = parseInt(rssUrlWithOptions[1]);
+    //check if rssItemsCountParam is a valid number
+    let rssItemsCount;
+    if(Number.isInteger(rssItemsCountParam)) {
+      rssItemsCount = rssItemsCountParam
+    } else {
+      rssItemsCount = null;
+    }
+    return {
+      rssUrl: rssUrlWithOptions[0],
+      rssItemsCount: rssItemsCount
+    }
+  });
+
+  rssUrlsWithOptionsArray.forEach(rssUrlWithOptions => {
     const data = {
-      rss_url: rss
+      rss_url: rssUrlWithOptions.rssUrl
     };
     axios
       .get("https://api.rss2json.com/v1/api.json", { params: data })
       .then(function(response) {
         // handle success
-        const posts = response.data.items;
+        let posts;
+        //take only needed items if rssItemsCount is valid
+        if(rssUrlWithOptions.rssItemsCount) {
+          posts = response.data.items.slice(0,rssUrlWithOptions.rssItemsCount);
+        } else {
+          posts = response.data.items;
+        }
         commit("addPosts", posts);
       })
       .catch(function(error) {
